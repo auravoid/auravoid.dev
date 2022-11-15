c622855df2d600acab6de6f1e9557fa762e6a11f
Jason <void206551@gmail.com>
2022-11-14 22:53:24 -0500

    whoopsies 😅

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
 export const HELLO = "World.";
 
 export const NAME = "auravoid";
+export const SITE_TITLE = "auravoid"
+export const SITE_DESCRIPTION = "A simple website. Nothing too special about it."
