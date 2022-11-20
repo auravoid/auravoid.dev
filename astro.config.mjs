efe33ed84e24c612df660b38ed5f5c5524625a79
Jason <void206551@gmail.com>
2022-11-20 18:34:32 -0500

    Linted

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://auravoid.dev",
-  integrations: [mdx(), sitemap()]
+  integrations: [mdx(), sitemap()],
 });
