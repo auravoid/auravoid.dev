af697fbe1e5caf0746ba17f66295ed59961b30d8
aura <32175146+auravoid@users.noreply.github.com>
2022-11-20 20:06:44 -0500

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const HELLO = "World.";
+
+export const NAME = "auravoid";
+export const SITE_TITLE = "auravoid";
+export const SITE_DESCRIPTION =
+  "A simple website. Nothing too special about it.";
