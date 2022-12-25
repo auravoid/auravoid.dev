27e542609fc3be8f8c4864434e361413c703355d
Jason <void206551@gmail.com>
2022-12-25 22:06:55 +0000

    Redesigning a bunch of shit ig

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://auravoid.dev",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()]
 });
