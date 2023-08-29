34c235ba80bff1d6858ea530563bbfd223c53fb7
aura <git@auravoid.dev>
2023-08-29 19:56:41 -0400

    Reset everything

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export const HELLO = "World."
-
-export const NAME = "auravoid"
-export const SITE_TITLE = "auravoid"
-export const SITE_DESCRIPTION =
-	"A simple website. Nothing too special about it."
